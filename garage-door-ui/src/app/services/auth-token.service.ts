afc26003f734b5d99fce7f07b6f0c92669c751bd
unknown <10414642+Roaders@users.noreply.github.com>
2020-05-20 12:05:22 +0100

    Store token in local storage

--- garage-door-ui/src/app/services/auth-token.service.ts
+++ garage-door-ui/src/app/services/auth-token.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
 import { IAuthToken } from '../../../../shared';
 
+const LOCAL_STORAGE_KEY = `rpi-garage-door.token.store`;
+
 @Injectable()
 export class AuthTokenService {
-    public authToken: IAuthToken | undefined;
+    public get authToken(): IAuthToken | undefined {
+        const rawString = localStorage.getItem(LOCAL_STORAGE_KEY);
+        return rawString != null ? JSON.parse(rawString) : undefined;
+    }
+
+    public set authToken(v: IAuthToken | undefined) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(v));
+    }
 }

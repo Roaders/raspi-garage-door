9905e65a391ad754eb97a11cade01e6733baa1bc
unknown <10414642+Roaders@users.noreply.github.com>
2020-05-27 07:23:54 +0100

    Define service options

--- rpi-garage-door/src/contracts.ts
+++ rpi-garage-door/src/contracts.ts
@@ -4,3 +4,38 @@ export type status = writeStatus | 'OPENING' | 'CLOSING' | 'UNKNOWN';
 export interface IGarageDoorStatus<T extends writeStatus | status = status> {
     readonly status: T;
 }
+
+export type MODE = 'mode_rpi' | 'mode_bcm';
+
+export interface IGarageDoorOptions {
+    /**
+     * how long to press the button for in ms
+     * defaults to 1 second
+     */
+    buttonPressInterval: number;
+    /**
+     * Pin addressing method.
+     * Defaults to 'mode_rpi'
+     */
+    pinAddressingMode: MODE;
+    /**
+     * pin to drive button press relay from
+     * defaults to 11 (BCM 17)
+     */
+    buttonPressRelayPin: number;
+    /**
+     * pin to attach door open switch to
+     * defaults to 13 (BCM 27)
+     */
+    doorOpenSwitchPin: number;
+    /**
+     * pin to attach door close pin to
+     * defaults to 15 (BCM 22)
+     */
+    doorClosedSwitchPin: number;
+    /**
+     * amount of time to wait once a switch is triggered before updateing state in ms
+     * defaults to 1 seconds
+     */
+    stateChangeDelay: number;
+}
--- rpi-garage-door/src/services/garage-door.service.ts
+++ rpi-garage-door/src/services/garage-door.service.ts
@@ -1,17 +1,32 @@
-import { IGarageDoorStatus, writeStatus } from '../contracts';
+import { IGarageDoorStatus, writeStatus, IGarageDoorOptions } from '../contracts';
 import { AsyncIterableServiceFactory, AsyncIterableService } from './async-iterable.service';
+import gpio from 'rpi-gpio';
 
 function unsupportedWrite(state: never): IGarageDoorStatus {
     throw new Error(`Could not set door status to '${state}'`);
 }
 
+const defaultOptions: IGarageDoorOptions = {
+    buttonPressInterval: 1000,
+    pinAddressingMode: 'mode_rpi',
+    buttonPressRelayPin: 11,
+    doorOpenSwitchPin: 13,
+    doorClosedSwitchPin: 15,
+    stateChangeDelay: 1000,
+};
+
 export class GarageDoorService {
-    constructor(factory: AsyncIterableServiceFactory<IGarageDoorStatus>) {
+    private readonly _options: IGarageDoorOptions;
+    private _status: IGarageDoorStatus | undefined;
+
+    constructor(factory: AsyncIterableServiceFactory<IGarageDoorStatus>, options?: Partial<IGarageDoorOptions>) {
         this.events = factory.create();
+        this._options = { ...defaultOptions, ...options };
+
+        gpio.setMode(this._options.pinAddressingMode);
     }
 
     public readonly events: AsyncIterableService<IGarageDoorStatus>;
-    private _status: IGarageDoorStatus | undefined;
 
     public async getState(): Promise<IGarageDoorStatus> {
         this._status = this._status || { status: 'UNKNOWN' };

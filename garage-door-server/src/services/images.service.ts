d1861afa720b67cf86471c84966daddb5aebfd56
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2020-07-19 08:54:58 +0100

    Handle concurrent calls to take a picture

--- garage-door-server/src/services/images.service.ts
+++ garage-door-server/src/services/images.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Optional } from '@nestjs/common';
 import { IGarageDoorStatus, DOOR_STATUS, isDefined, IStatusChangeImage } from '../../../shared';
 import { GarageDoorService } from '../../../rpi-garage-door/src';
 import { Observable, from, defer, Subject, empty, of } from 'rxjs';
-import { mergeMap, catchError, map, distinctUntilKeyChanged, filter } from 'rxjs/operators';
+import { mergeMap, catchError, map, distinctUntilKeyChanged, filter, shareReplay, tap } from 'rxjs/operators';
 import { PiCameraFactory } from 'src/factory';
 import { promises as fsPromises } from 'fs';
 import { createImageDTO } from '../helpers';
@@ -12,6 +12,8 @@ const imageFileNameRegExp = /^(\d+)_(\w+).jpg$/;
 @Injectable()
 export class ImagesService {
     private readonly statusSubject = new Subject<IGarageDoorStatus<DOOR_STATUS>>();
+
+    private _snapInProgress: Observable<string> | undefined;
 
     constructor(private garageDoorService: GarageDoorService, @Optional() private cameraFactory?: PiCameraFactory) {
         this.listenForEvents();
@@ -63,15 +65,26 @@ export class ImagesService {
     }
 
     public snap(status: IGarageDoorStatus): Observable<string> {
+        if (this._snapInProgress != null) {
+            return this._snapInProgress;
+        }
+
         const cameraFactory = this.cameraFactory;
         if (cameraFactory == null) {
             return empty();
         }
 
-        return defer(() => {
+        const snapInProgress = defer(() => {
             const fileName = `${Date.now()}_${status.status}.jpg`;
-            return from(cameraFactory.create(fileName).snap()).pipe(map(() => fileName));
-        });
+            return from(cameraFactory.create(fileName).snap()).pipe(
+                tap(() => (this._snapInProgress = undefined)),
+                map(() => fileName),
+            );
+        }).pipe(shareReplay(1));
+
+        this._snapInProgress = snapInProgress;
+
+        return snapInProgress;
     }
 
     private async listenForEvents() {
